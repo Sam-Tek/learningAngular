24aaedca302eb4fb4cc7ab0342c1129614ad1062
Sam.Tek <sonny.foot@hotmail.com>
2021-08-12 15:56:50 +0200

    prepare the project to see the lesson on the rooting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  isAuth = false;
-
-  lastUpdate = new Date();
-  lastUpdateAsync = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
-
-  appareils!: any[];
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-
-  ngOnInit(){
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre() {
-    if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
-      this.appareilService.switchOffAll();
-      return 1;
-    } else {
-      return null;
-    }
+export class AppComponent implements OnInit{
+  constructor(){}
+  ngOnInit(): void {
   }
 }
